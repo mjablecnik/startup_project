1c0d04f71ef1a37fa1b677daa3aad54c56ec9cb3
Martin Jablečník <martin.jablecnik@email.cz>
2024-09-21 23:22:30 +0200

    refactor: main function

--- scripts/cli_template/lib/main.dart
+++ scripts/cli_template/lib/main.dart
@@ -1,25 +1,36 @@
+import 'package:cli_template/app_info.dart';
+
 import 'argument_parser.dart';
 
 /*
 
 Example of usage:
-dart lib/main.dart -V -h --version --name Martin -t dart,python,java
+dart lib/main.dart -V --name Martin -t dart,python,java
 
  */
-Future<void> main(List<String> args) async {
-  Arguments.parse(args).then((args) {
-    print("Verbose: ${args.isVerbose}");
-    print("Version: ${args.showVersion}");
-    print("Help: ${args.showHelp}");
-    print("Name: ${args.name}");
-    print("Tags: ${args.tags}");
+void main(List<String> args) {
+  Arguments.parse(args).then((args) async {
+    if (args.showHelp) {
+      showHelp();
+    } else if (args.showVersion) {
+      print("Version: ${await AppInfo().version}");
+    } else {
+      print("Verbose: ${args.isVerbose}");
+      print("Name: ${args.name}");
+      print("Tags: ${args.tags}");
+    }
   }, onError: (error) {
     if (error is FormatException) {
       print(error.message);
-      print("\nUsage:");
-      print(Arguments.usage);
+      showHelp();
       return;
     }
     print(error);
   });
+}
+
+
+showHelp() {
+  print("\nUsage:");
+  print(Arguments.usage);
 }
