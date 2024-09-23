058e369dae751a962a4426ca49bb314ea952ff67
Martin Jablečník <martin.jablecnik@email.cz>
2024-09-23 21:43:26 +0200

    refactor(cli_script): refactor AppInfo

--- scripts/cli_template/lib/app_info.dart
+++ scripts/cli_template/lib/app_info.dart
@@ -2,29 +2,28 @@ import 'dart:io';
 import 'package:yaml/yaml.dart';
 
 class AppInfo {
-  dynamic _getPubspec() async {
-    final file = File('${_projectRoot()}/pubspec.yaml');
-    final yamlString = await file.readAsString();
+  dynamic get _pubspec {
+    final file = File('${_projectRoot(file: 'pubspec.yaml')}/pubspec.yaml');
+    final yamlString = file.readAsStringSync();
     return loadYaml(yamlString);
   }
 
-  String _projectRoot() {
+  List<String> get gitScope {
+    final file = File('${_projectRoot(file: '.gitscope')}/.gitscope');
+    return file.readAsLinesSync();
+  }
+
+  String _projectRoot({required String file}) {
     Directory root = Directory.current;
 
-    while (root.path.isNotEmpty && !File('${root.path}/pubspec.yaml').existsSync()) {
+    while (root.path.isNotEmpty && !File('${root.path}/$file').existsSync()) {
       root = root.parent;
     }
 
     return root.path;
   }
 
-  Future<String> get version async {
-    final pubspec = await _getPubspec();
-    return pubspec['version'];
-  }
+  Future<String> get version => _pubspec['version'];
 
-  Future<String> get name async {
-    final pubspec = await _getPubspec();
-    return pubspec['name'];
-  }
+  Future<String> get name  => _pubspec['name'];
 }
