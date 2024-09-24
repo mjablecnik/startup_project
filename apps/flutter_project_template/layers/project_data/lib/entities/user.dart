cb5bc859917191c76960483841e7cb396fea825e
Martin Jablečník <martin.jablecnik@email.cz>
2024-09-24 02:35:41 +0200

    feat(flutter_app): add data layer

---
+++ apps/flutter_project_template/layers/project_data/lib/entities/user.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  const User._();
+
+  const factory User({
+    required String firstName,
+    required String lastName,
+    @JsonKey(name: "username") required String userName,
+    String? token,
+  }) = _User;
+
+  factory User.anonymous() {
+    return const User(
+      userName: "anonymous",
+      lastName: "User",
+      firstName: "Anonymous",
+    );
+  }
+
+  String get fullName => "$firstName $lastName";
+
+  factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
+}
